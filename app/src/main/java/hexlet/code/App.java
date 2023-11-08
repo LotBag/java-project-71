8292ab8df359ee1a3f381a8faafe860295c7420e
LotBag <hexlet@DESKTOP-2AEVM0K>
2023-11-08 19:23:18 +0700

    add option and parameters

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,10 +4,20 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
+import java.io.File;
 
 @CommandLine.Command(name = "gendiff", version = "gendiff 0.1",
         description = "Compares two configuration files and shows a difference.", mixinStandardHelpOptions = true)
 public class App implements Runnable {
+
+    @Option(names = { "-f", "--format" }, description = "output format [default: stylish]") // |3|
+    String format = "stylish";
+
+    @Parameters(paramLabel = "filepath1", index = "0", description = "The file whose checksum to calculate.")
+    File filepath1;
+    @Parameters(paramLabel = "filepath2", index = "1", description = "The file whose checksum to calculate.")
+    File filepath2;
+
     @Override
     public void run() {
         System.out.println("Hello World!");
