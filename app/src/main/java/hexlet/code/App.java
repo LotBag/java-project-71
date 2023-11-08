3712d9466be3c0cbdb51f9b980a2b8228cc5191b
LotBag <hexlet@DESKTOP-2AEVM0K>
2023-11-08 16:18:11 +0700

    add picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,20 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@CommandLine.Command(name = "gendiff", version = "gendiff 0.1",
+        description = "Compares two configuration files and shows a difference.", mixinStandardHelpOptions = true)
+public class App implements Runnable {
+    @Override
+    public void run() {
         System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
