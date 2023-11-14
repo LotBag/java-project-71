0ab6fd1265f2bda80acceaf74f19de018a538adf
LotBag <hexlet@DESKTOP-2AEVM0K>
2023-11-15 00:40:39 +0700

    add generate

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,30 +1,30 @@
 package hexlet.code;
 
 import picocli.CommandLine;
-import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
-import java.io.File;
+import java.util.concurrent.Callable;
 
-@CommandLine.Command(name = "gendiff", version = "gendiff 0.1",
+@CommandLine.Command(name = "gendiff", version = "gendiff very early access",
         description = "Compares two configuration files and shows a difference.", mixinStandardHelpOptions = true)
-public class App implements Runnable {
+public class App implements Callable<String> {
 
-    @Option(names = { "-f", "--format" }, description = "output format [default: stylish]") // |3|
+    @Option(names = { "-f", "--format" }, description = "output format [default: stylish]")
     String format = "stylish";
 
-    @Parameters(paramLabel = "filepath1", index = "0", description = "The file whose checksum to calculate.")
-    File filepath1;
-    @Parameters(paramLabel = "filepath2", index = "1", description = "The file whose checksum to calculate.")
-    File filepath2;
+    @Parameters(paramLabel = "filepath1", index = "0", description = "Path to first file.")
+    static String filepath1;
+    @Parameters(paramLabel = "filepath2", index = "1", description = "Path to second file.")
+    static String filepath2;
 
     @Override
-    public void run() {
-        System.out.println("Hello World!");
+    public String call() throws Exception {
+        return Differ.generate(filepath1, filepath2);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
     }
+
 }
---
+++ app/src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,58 @@
+package hexlet.code;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Differ {
+
+    static Map<String, String> dataMap1 = new TreeMap<>();
+    static Map<String, String> dataMap2 = new TreeMap<>();
+    static Map<String, String> resultMap = new LinkedHashMap<>();
+
+    public static String generate(String filepath1, String filepath2) throws Exception {
+        dataMap1 = Data.dataToMap(filepath1);
+        dataMap2 = Data.dataToMap(filepath2);
+
+        for (var key : dataMap1.keySet()) {
+            if (dataMap2.containsKey(key)) {
+                if (dataMap1.get(key).equals(dataMap2.get(key))) {
+                    // Without changes
+                    resultMap.put("  " + key, dataMap1.get(key));
+                } else {
+                    // The value has changed
+                    resultMap.put(("- " + key), dataMap1.get(key));
+                    resultMap.put(("+ " + key), dataMap2.get(key));
+                }
+            } else {
+                // Key and value removed
+                resultMap.put(("- " + key), dataMap1.get(key));
+            }
+
+        }
+
+        for (var key : dataMap2.keySet()) {
+            if (!dataMap1.containsKey(key)) {
+                //Key and value add
+                resultMap.put(("+ " + key), dataMap2.get(key));
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for (var key : resultMap.keySet()) {
+            result.append(key)
+                    .append(" ")
+                    .append(resultMap.get(key))
+                    .append("\n");
+        }
+
+        result = new StringBuilder("{" + result + "}");
+
+        System.out.println(result);
+
+        return String.valueOf(result);
+
+    }
+
+}
